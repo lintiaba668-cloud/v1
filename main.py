9fa89d0653ece40485bed5d0063885e118afcd18
lintiaba668-cloud <lintiaba668@gmail.com>
2026-07-18 00:15:20 +0800

    程序入口切换PyQt5，兼容Win7版本

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """
 PowerRename V1 程序入口
+Win7兼容版
 """
 
 import sys
 
-from PySide6.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication
 
 from ui.main_window_v3 import MainWindowV3
 
@@ -13,4 +14,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindowV3()
     window.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
