4f5f0928f8bc90e6f9db39e0dc487b42e535d6dd
lintiaba668-cloud <lintiaba668@gmail.com>
2026-07-22 13:57:00 +0800

    feat: add portable config module

---
+++ build/config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Portable application configuration.
+"""
+
+APP_NAME = 'PowerRename V1'
+APP_VERSION = '1.0'
+
+DEFAULT_DATABASE = 'data/projects.db'
+DEFAULT_BACKUP_DIR = 'backup'
+DEFAULT_MODEL_DIR = 'models'
