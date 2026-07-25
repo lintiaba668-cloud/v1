625fadfefc9441dc7430563560ba1bd9ce2a7b24
lintiaba668-cloud <lintiaba668@gmail.com>
2026-07-25 16:39:20 +0800

    feat: carry template OCR candidates and report metadata

--- core/ocr_result.py
+++ core/ocr_result.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""OCR result data object.
-
-Internal structured result container while keeping dictionary compatibility.
-"""
+"""OCR result data object with dictionary compatibility."""
 
 
 class OCRResult:
@@ -15,6 +12,11 @@ class OCRResult:
         items=None,
         project_name='',
         project_code='',
+        project_name_candidates=None,
+        project_code_candidates=None,
+        report_type='',
+        rotation_angle=0,
+        recognition_source='',
         status='',
         error_code=0,
         error_message=''
@@ -24,6 +26,11 @@ class OCRResult:
         self.items = items or []
         self.project_name = project_name
         self.project_code = project_code
+        self.project_name_candidates = project_name_candidates or []
+        self.project_code_candidates = project_code_candidates or []
+        self.report_type = report_type
+        self.rotation_angle = rotation_angle
+        self.recognition_source = recognition_source
         self.status = status
         self.error_code = error_code
         self.error_message = error_message
@@ -35,6 +42,11 @@ class OCRResult:
             'items': self.items,
             'project_name': self.project_name,
             'project_code': self.project_code,
+            'project_name_candidates': self.project_name_candidates,
+            'project_code_candidates': self.project_code_candidates,
+            'report_type': self.report_type,
+            'rotation_angle': self.rotation_angle,
+            'recognition_source': self.recognition_source,
             'status': self.status,
             'error_code': self.error_code,
             'error_message': self.error_message
